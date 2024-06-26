556bd5936d95e56fe254c2a7529cfe1b9270f06e
William Tavares <williamtavaresdemoura@gmail.com>
2024-06-26 14:12:48 -0300

    Definindo escopo de classes

---
+++ classes.py
@@ -0,0 +1,24 @@
+class Torre:
+      class Apartamento:
+            def __init__(self, numero, torre, vaga):
+                  self.id = 0
+                  self.numero = numero
+                  self.torre = torre
+                  self.vaga = 0
+                  self.proximo = None
+                  
+            def cadastrar(self):
+                  pass
+            def imprimir(self):
+                  pass
+            
+      def __init__(self, nome, endereco):
+            self.id = 0
+            self.nome = nome
+            self.endereco = endereco
+            
+      def cadastrar(self):
+            pass
+      def imprimir(self):
+            pass
+            
